d27d1b994c9214cc79195a6812161e5c4dc972a1
rnfkehdrlf <rnfkehdrlf@gmail.com>
2022-11-05 00:56:06 +0900

    add 004 quiz

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        quiz003();
+    public static void main(String[] args){
+        try {
+            quiz004();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    static void quiz003() throws IOException {
+    static void quiz004() throws IOException {
         /*
         문제
-        수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+        N×N개의 수가 N×N 크기의 표에 채워져 있다. (x1, y1)부터 (x2, y2)까지 합을 구하는 프로그램을 작성하시오. (x, y)는 x행 y열을 의미한다.
+        예를 들어, N = 4이고, 표가 아래와 같이 채워져 있는 경우를 살펴보자.
+        1	2	3	4
+        2	3	4	5
+        3	4	5	6
+        4	5	6	7
+        여기서 (2, 2)부터 (3, 4)까지 합을 구하면 3+4+5+4+5+6 = 27이고, (4, 4)부터 (4, 4)까지 합을 구하면 7이다.
+        표에 채워져 있는 수와 합을 구하는 연산이 주어졌을 때, 이를 처리하는 프로그램을 작성하시오.
+
         입력
-        첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+        첫째 줄에 표의 크기 N과 합을 구해야 하는 횟수 M이 주어진다. (1 ≤ N ≤ 1024, 1 ≤ M ≤ 100,000)
+        둘째 줄부터 N개의 줄에는 표에 채워져 있는 수가 1행부터 차례대로 주어진다.
+        다음 M개의 줄에는 네 개의 정수 x1, y1, x2, y2 가 주어지며, (x1, y1)부터 (x2, y2)의 합을 구해 출력해야 한다.
+        표에 채워져 있는 수는 1,000보다 작거나 같은 자연수이다. (x1 ≤ x2, y1 ≤ y2)
+
         출력
-        총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
-        제한
-        1 ≤ N ≤ 100,000
-        1 ≤ M ≤ 100,000
-        1 ≤ i ≤ j ≤ N
+        총 M줄에 걸쳐 (x1, y1)부터 (x2, y2)까지 합을 구해 출력한다.
          */
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int suNo = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
 
-        int[] S = new int[suNo+1];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int sum = 0;
-        for (int i =1; i<=suNo; i++) {
-            sum += Integer.parseInt(stringTokenizer.nextToken());
-            S[i] = sum;
-        }
-        for (int k = 0; k<quizNo; k++) {
+        long[][] A = new long[N+1][N+1];
+        for (int i =1; i<=N; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j] - S[i-1]);
+            for (int j =1; j<=N; j++) {
+                A[i][j] = Long.parseLong(stringTokenizer.nextToken());
+            }
+        }
+        long[][] S = new long[N+1][N+1];
+        for (int i = 1; i<=N; i++) {
+            for (int j =1; j<=N; j++) {
+                S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + A[i][j];
+            }
+        }
+        for (int k = 0; k<M; k++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x1 = Integer.parseInt(stringTokenizer.nextToken());
+            int y1 = Integer.parseInt(stringTokenizer.nextToken());
+            int x2 = Integer.parseInt(stringTokenizer.nextToken());
+            int y2 = Integer.parseInt(stringTokenizer.nextToken());
+            long result = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1];
+            System.out.println(result);
         }
     }
 }
