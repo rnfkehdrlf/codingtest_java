4c717b528b63cc36a8acb3699ee8e0b00dd188e7
rnfkehdrlf <rnfkehdrlf@gmail.com>
2022-11-07 21:08:52 +0900

    add 005 quiz

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args){
         try {
-            quiz004();
+            quiz005();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static void quiz004() throws IOException {
-        /*
-        문제
-        N×N개의 수가 N×N 크기의 표에 채워져 있다. (x1, y1)부터 (x2, y2)까지 합을 구하는 프로그램을 작성하시오. (x, y)는 x행 y열을 의미한다.
-        예를 들어, N = 4이고, 표가 아래와 같이 채워져 있는 경우를 살펴보자.
-        1	2	3	4
-        2	3	4	5
-        3	4	5	6
-        4	5	6	7
-        여기서 (2, 2)부터 (3, 4)까지 합을 구하면 3+4+5+4+5+6 = 27이고, (4, 4)부터 (4, 4)까지 합을 구하면 7이다.
-        표에 채워져 있는 수와 합을 구하는 연산이 주어졌을 때, 이를 처리하는 프로그램을 작성하시오.
+    static void quiz005() throws IOException {
+/*
+문제
+수 N개 A1, A2, ..., AN이 주어진다. 이때, 연속된 부분 구간의 합이 M으로 나누어 떨어지는 구간의 개수를 구하는 프로그램을 작성하시오.
 
-        입력
-        첫째 줄에 표의 크기 N과 합을 구해야 하는 횟수 M이 주어진다. (1 ≤ N ≤ 1024, 1 ≤ M ≤ 100,000)
-        둘째 줄부터 N개의 줄에는 표에 채워져 있는 수가 1행부터 차례대로 주어진다.
-        다음 M개의 줄에는 네 개의 정수 x1, y1, x2, y2 가 주어지며, (x1, y1)부터 (x2, y2)의 합을 구해 출력해야 한다.
-        표에 채워져 있는 수는 1,000보다 작거나 같은 자연수이다. (x1 ≤ x2, y1 ≤ y2)
+즉, Ai + ... + Aj (i ≤ j) 의 합이 M으로 나누어 떨어지는 (i, j) 쌍의 개수를 구해야 한다.
 
-        출력
-        총 M줄에 걸쳐 (x1, y1)부터 (x2, y2)까지 합을 구해 출력한다.
-         */
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ N ≤ 106, 2 ≤ M ≤ 103)
 
-        long[][] A = new long[N+1][N+1];
-        for (int i =1; i<=N; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int j =1; j<=N; j++) {
-                A[i][j] = Long.parseLong(stringTokenizer.nextToken());
+둘째 줄에 N개의 수 A1, A2, ..., AN이 주어진다. (0 ≤ Ai ≤ 109)
+
+출력
+첫째 줄에 연속된 부분 구간의 합이 M으로 나누어 떨어지는 구간의 개수를 출력한다.
+*/
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long answer = 0;
+        S[0] = sc.nextInt();
+        for (int i =1; i<N; i++) { //수열 합 배열 만들기
+            S[i] = S[i-1] + sc.nextInt();
+        }
+        for(int i =0; i<N; i++) {   // 합 배열의 모든 값에 %연산 수행하기
+            int remainder = (int) (S[i]%M);
+            // 0 ~ i까지의 구간 합 자체가 0일때 정답에 더하기.
+            if (remainder == 0) answer++;
+            // 나머지가 같은 인덱스의 개수 카운팅하기.
+            C[remainder] += 1;
+        }
+        for (int i = 1; i<M; i++) {
+            if (C[i]>1) {
+                // 나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기.
+                answer = answer + (C[i] * (C[i]-1) /2);
             }
         }
-        long[][] S = new long[N+1][N+1];
-        for (int i = 1; i<=N; i++) {
-            for (int j =1; j<=N; j++) {
-                S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + A[i][j];
-            }
-        }
-        for (int k = 0; k<M; k++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int x1 = Integer.parseInt(stringTokenizer.nextToken());
-            int y1 = Integer.parseInt(stringTokenizer.nextToken());
-            int x2 = Integer.parseInt(stringTokenizer.nextToken());
-            int y2 = Integer.parseInt(stringTokenizer.nextToken());
-            long result = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1];
-            System.out.println(result);
-        }
+        System.out.println(answer);
     }
 }
