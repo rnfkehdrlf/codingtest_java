82723db0a704dc9ba47ff15e6aa19bbc9fe23691
alex <alex.kang@kinemaster.com>
2022-11-28 00:25:48 +0900

    quiz 013 commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,60 +1,46 @@
 import java.util.*
+import kotlin.collections.ArrayDeque
 
 fun main(args: Array<String>) {
 /*
-* 문제
-크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다. Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+문제
+N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
 
-예를 들어, A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다. A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
+이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
+
+N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+첫째 줄에 정수 N(1 ≤ N ≤ 500,000)이 주어진다.
 
 출력
-총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
+첫째 줄에 남게 되는 카드의 번호를 출력한다.
 
 예제 입력 1
-4
-3 5 2 7
+6
 예제 출력 1
-5 7 7 -1
-예제 입력 2
 4
-9 5 4 8
-예제 출력 2
--1 8 8 -1
-* */
-    quiz012()
+*/
+    quiz013()
 }
 
-fun quiz012() {
+fun quiz013() {
 
 
     val sc = Scanner(System.`in`)
     val N = sc.nextInt()
-    val A = IntArray(N) {
-        sc.nextInt()
-    }
-    val stack = Stack<Int>()
-    val result = IntArray(N)
 
-    stack.push(0)
-    for (i in A.indices) {
-        while (stack.isNotEmpty() && A[stack.peek()] < A[i]) {
-            result[stack.pop()] = A[i]
-        }
-        stack.push(i)
+    val queue = ArrayDeque<Int>()
 
+    for (i in 1 .. N) {
+        queue.addLast(i)
     }
-    for (i in result.indices) {
-        if (result[i] == 0) result[i] = -1
+
+    while (queue.size > 1) {
+        queue.removeFirst()
+        queue.addLast(queue.removeFirst())
     }
-    /*while (stack.isNotEmpty()) {
-        result[stack.pop()] = -1
-    }*/
-    val bf = StringBuffer()
-    for (i in result.indices) {
-        bf.append(result[i].toString() + " ")
-    }
-    println(bf)
+    println(queue.removeFirst())
 }
