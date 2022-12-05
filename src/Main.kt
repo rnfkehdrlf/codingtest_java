4692cfb8bb63660a9fdf70d24f9cc0288e6e31f5
alex <alex.kang@kinemaster.com>
2022-12-05 13:50:03 +0900

    quiz 015 commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,85 +1,53 @@
 import java.util.*
-import kotlin.collections.ArrayDeque
 
 fun main(args: Array<String>) {
 /*
 문제
-절댓값 힙은 다음과 같은 연산을 지원하는 자료구조이다.
-
-배열에 정수 x (x ≠ 0)를 넣는다.
-배열에서 절댓값이 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다. 절댓값이 가장 작은 값이 여러개일 때는, 가장 작은 수를 출력하고, 그 값을 배열에서 제거한다.
-프로그램은 처음에 비어있는 배열에서 시작하게 된다.
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 연산의 개수 N(1≤N≤100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 0이 아니라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 정수는 -231보다 크고, 231보다 작다.
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 
 출력
-입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 절댓값이 가장 작은 값을 출력하라고 한 경우에는 0을 출력하면 된다.
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
 예제 입력 1
-18
-1
--1
-0
-0
-0
-1
-1
--1
--1
+5
+5
 2
--2
-0
-0
-0
-0
-0
-0
-0
+3
+4
+1
 예제 출력 1
--1
 1
-0
--1
--1
-1
-1
--2
 2
-0
+3
+4
+5
 */
-    quiz014()
+    quiz015()
 }
 
-fun quiz014() {
+fun quiz015() {
 
 
     val sc = Scanner(System.`in`)
     val N = sc.nextInt()
-
-
-    val pq = PriorityQueue<Int> {o1,o2 ->
-        val firstAbs = Math.abs(o1)
-        val secondAbs = Math.abs(o2)
-        if (firstAbs == secondAbs) {
-            return@PriorityQueue if (o1 > o2) 1 else -1
-        } else {
-            return@PriorityQueue firstAbs - secondAbs
-        }
+    var A = IntArray(N) {
+        sc.nextInt()
     }
-    for (i in 0 until  N) {
-        val value = sc.nextInt()
-        if (value == 0) {
-            //출력 first
-            if (pq.isEmpty())
-                println("0")
-            else
-                println(pq.poll())
-        } else {
-            //add last
-            pq.add(value)
+
+    for (i in 0 until N) {
+        for (j in 0 until  N-1-i){
+            if (A[j] > A[j+1]) {
+                val temp = A[j]
+                A[j] = A[j+1]
+                A[j+1] = temp
+            }
         }
     }
 
-
+    for (i in A.indices) {
+        println(A[i])
+    }
 }
